94eaa1dc15b18853be1022f8106d5c06d97aa612
pokenDev <85866987+pokenDev@users.noreply.github.com>
2021-10-07 16:14:11 +0530

    Add ability to handle deflationary nature of PKN

--- pooling/PKNPool.sol
+++ pooling/PKNPool.sol
@@ -14,16 +14,17 @@ contract PooledPKN is ERC20("PooledPKN", "pPKN") {
 
     // Locks PKN and mints pPKN
     function enter(uint256 _amount) public {
+        uint256 _actualAmount = _receivePKN(msg.sender, _amount);
+
         uint256 totalPKN = PKN.balanceOf(address(this));
         uint256 totalShares = totalSupply();
         if (totalShares == 0 || totalPKN == 0) {
-            _mint(msg.sender, _amount);
+            _mint(msg.sender, _actualAmount);
         } 
         else {
-            uint256 what = (_amount * totalShares) / totalPKN;
+            uint256 what = (_actualAmount * totalShares) / totalPKN;
             _mint(msg.sender, what);
         }
-        PKN.transferFrom(msg.sender, address(this), _amount);
     }
 
     function pknPooledByUser(address user) public view returns(uint256) {
@@ -43,5 +44,12 @@ contract PooledPKN is ERC20("PooledPKN", "pPKN") {
 
     function _transfer(address sender, address recipient, uint256 amount) internal pure override {
         require(false, "pPKN: No transfers allowed");
+    }
+
+    function _receivePKN(address from, uint256 amount) internal returns(uint256) {
+        uint256 balanceBefore = PKN.balanceOf(address(this));
+        PKN.transferFrom(from, address(this), amount);
+        uint256 balanceAfter = PKN.balanceOf(address(this));
+        return balanceAfter - balanceBefore;
     }
 }
